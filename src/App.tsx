e26497345b988e44ac9ac4b5140fb2cee285ca6e
Oleg Albekov <oleg@albekov.net>
2025-09-28 08:01:39 +0400

    feat: analyze text

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import { Container, Grid, Textarea } from "@mantine/core";
+import { Box, Container, Grid, Text, Textarea } from "@mantine/core";
+import { useDebounce } from "@uidotdev/usehooks";
+import { Tiktoken } from "js-tiktoken/lite";
+import o200k_base from "js-tiktoken/ranks/o200k_base";
+import { type ChangeEvent, useMemo, useState } from "react";
+
+const DEBOUNCE_DELAY = 1000;
+
+const enc = new Tiktoken(o200k_base);
 
 function App() {
+  const [text, setText] = useState<string>("");
+
+  const handleChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.currentTarget.value);
+  };
+
+  const debouncedText = useDebounce(text, DEBOUNCE_DELAY);
+
+  const textData = useMemo(() => {
+    const tokens = enc.encode(debouncedText);
+    return {
+      length: debouncedText.length,
+      tokens: tokens,
+    };
+  }, [debouncedText]);
+
   return (
     <Container style={{ padding: "1rem" }}>
       <Grid>
         <Grid.Col span={{ base: 12, md: 9 }}>
-          <Textarea autosize minRows={10} />
+          <Textarea
+            autosize
+            minRows={10}
+            value={text}
+            onChange={handleChangeText}
+            placeholder="Enter text here..."
+          />
         </Grid.Col>
 
-        <Grid.Col span={{ base: 12, md: 3 }}></Grid.Col>
+        <Grid.Col span={{ base: 12, md: 3 }}>
+          <Box mb="sm">
+            <Text span fw={700}>
+              Length:
+            </Text>{" "}
+            <Text span>{textData.length}</Text>
+          </Box>
+          <Box mb="sm">
+            <Text span fw={700}>
+              Tokens:
+            </Text>{" "}
+            <Text span>{textData.tokens.length}</Text>
+          </Box>
+        </Grid.Col>
       </Grid>
     </Container>
   );
