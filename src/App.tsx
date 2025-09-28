f9c39653519044d41df96ea95f83a6788b679902
Oleg Albekov <oleg@albekov.net>
2025-09-28 07:35:49 +0400

    chore: add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Container, Grid, Textarea } from "@mantine/core";
+
 function App() {
   return (
-    <>
-      <p>tokens</p>
-    </>
+    <Container style={{ padding: "1rem" }}>
+      <Grid>
+        <Grid.Col span={{ base: 12, md: 9 }}>
+          <Textarea autosize minRows={10} />
+        </Grid.Col>
+
+        <Grid.Col span={{ base: 12, md: 3 }}></Grid.Col>
+      </Grid>
+    </Container>
   );
 }
 
